40b599bed2308788365eb0cdee5061287a8c2fc9
spatterss135 <spatterss135@gmail.com>
2022-01-31 19:43:53 -0600

    klklk

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,29 +1,15 @@
 
 import {useEffect, useState } from "react"
 
-export default function Books() {
-    let [data, setData] = useState([])
+export default function Books({handleFetch}) {
 
-    const endpoint =  'https://the-one-api.dev/v2'
+    
 
-    useEffect(() => {
-        let fetchData = async () => {
-            let response = await fetch(`${endpoint}/movie`, {
-                headers: {
-                    Authorization: `Bearer QG6hwAvDDV3WtknfWiAL`
-                }
-            })
-            let rData = await response.json()
-            console.log(rData)
-        }
-        fetchData()
-    })
+ 
+
+
 
     return(
-        <>
-        
-        Hi
-
-        </>
+        <button onClick={(e) => handleFetch(e, 'book')}>Books</button>
     )
 }
