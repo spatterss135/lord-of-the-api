33e7a4a943b5408834ee966522b4e9c92be6ff42
spatterss135 <spatterss135@gmail.com>
2022-01-31 20:43:08 -0600

    Added Buttons

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,15 +1,26 @@
 
 import {useEffect, useState } from "react"
 
-export default function Books({handleFetch}) {
-
+export default function Books({handleFetch, data}) {
+    console.log(data)
     
-
- 
-
+    let names;
+    if (data.docs){
+        names = data.docs.map(book => {
+            return(
+                <div>
+                    <button onClick={(e) => handleFetch(e, `book/${book._id}/chapter`)}>{book.name?book.name:book.chapterName}</button>  
+                </div> )})
+    }
 
 
     return(
-        <button onClick={(e) => handleFetch(e, 'book')}>Books</button>
+        <div>
+<button onClick={(e) => handleFetch(e, 'book')}>Books</button>
+        <div>{names}</div>
+        </div>
+        
+
+
     )
 }
