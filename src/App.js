4d41ef18e27e7cbf699649f2f28a2f46e3858145
spatterss135 <spatterss135@gmail.com>
2022-01-31 19:25:58 -0600

    ll

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 // Components
@@ -7,6 +7,26 @@ import Characters from './components/Characters';
 import Movies from './components/Movies';
 
 function App() {
+
+    let [data, setData] = useState([])
+
+    const endpoint =  'https://the-one-api.dev/v2'
+
+
+  useEffect(() => {
+    let fetchData = async () => {
+        let response = await fetch(`${endpoint}/movie`, {
+            headers: {
+                Authorization: `Bearer QG6hwAvDDV3WtknfWiAL`
+            }
+        })
+        let rData = await response.json()
+        console.log(rData)
+    }
+    fetchData()
+})
+
+
   return (
     <div className="App">
       <Books />
